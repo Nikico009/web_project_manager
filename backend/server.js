11f56ebb4d6228b50bf6c432664ba889aee5d361
nikico009 <nikicomba2006@gmail.com>
2026-01-20 04:50:54 -0300

    Working first 2 real endpoints for posting projects and getting projects list

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,36 @@ const app = express(); // Ejecuto el "constructor" de express y llamo a la insta
 
 const PORT = 8080; // define
 
+// "Middleware"
+app.use(express.json()); // Parseo automaticamente los req q vengan en formato json, y luego puedo acceder a sus "categorías" como "atributos"
 
-app.get("/ping", (req, res) => {
-  res.send("pong");
+
+// Test Endpoints
+app.get("/test", (req, res) => {
+  res.send("server working correctly");
 })
+
+app.post("/echo_test", (req, res) => {
+  res.send("server recived: " + req.body.msg + "\n");
+})
+
+// Post and Get endpoints
+
+let proyectos = []; // Temporary array to store proyects
+
+app.get("/get_prj", (req, res) => {
+  res.send(proyectos + "\n");
+  console.log("User requested project list");
+})
+
+app.post("/post_prj", (req, res) => {
+  proyectos.push(req.body.prj);
+  res.send("\nserver stored proyect: " + req.body.prj + "\n");
+  console.log("User posted project: " + req.body.prj + "\n");
+})
+
+
+
 
 app.listen(PORT, () => {
     console.log("Server running on port" + PORT)
